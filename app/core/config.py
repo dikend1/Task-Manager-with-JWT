ec952a706d983afb9c786492115e43ea664629a9
Dias Maksatov <diasmaksatov@MacBook-Air-Dias.local>
2025-10-28 02:54:10 +0500

    update config

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL:str = "postgresql+psycopg2://postgres:123@localhost:5432/managerJWT"
